e1ab6ec45841ae61f8be083a8bae31806749952f
Jan Niklas Schreder <janniklas.schreder@gmx.de>
2025-06-13 14:22:56 +0200

    Refactor Transaktion class: add getters and setters for transaction properties

--- Bank/src/main/java/de/hsnr/bank/entities/Transaktion.java
+++ Bank/src/main/java/de/hsnr/bank/entities/Transaktion.java
@@ -19,4 +19,55 @@ public class Transaktion {
     public Transaktion() {
     }
     public Transaktion(Bankkonto konto, String transaktionsnummer, java.util.Date transaktionsdatum, double
+            betrag, String transaktionsart, String empfänger, String transaktionsstatus) {
+        this.konto = konto;
+        this.transaktionsnummer = transaktionsnummer;
+        this.transaktionsdatum = transaktionsdatum;
+        this.betrag = betrag; 
+        this.transaktionsart = transaktionsart;
+        this.empfänger = empfänger;
+        this.transaktionsstatus = transaktionsstatus;
+            }
+    public Bankkonto getKonto() {
+        return konto;
+    }   
+    public void setKonto(Bankkonto konto) {
+        this.konto = konto;
+    }
+    public String getTransaktionsnummer() {
+        return transaktionsnummer;
+    }
+    public void setTransaktionsnummer(String transaktionsnummer) {
+        this.transaktionsnummer = transaktionsnummer;
+    }
+    public java.util.Date getTransaktionsdatum() {
+        return transaktionsdatum;
+    }
+    public void setTransaktionsdatum(java.util.Date transaktionsdatum) {
+        this.transaktionsdatum = transaktionsdatum;
+    }
+    public double getBetrag() {
+        return betrag;
+    }
+    public void setBetrag(double betrag) {
+        this.betrag = betrag;
+    }
+    public String getTransaktionsart() {
+        return transaktionsart;
+    }
+    public void setTransaktionsart(String transaktionsart) {
+        this.transaktionsart = transaktionsart;
+    }
+    public String getEmpfänger() {
+        return empfänger;
+    }
+    public void setEmpfänger(String empfänger) {
+        this.empfänger = empfänger;
+    }
+    public String getTransaktionsstatus() {
+        return transaktionsstatus;
+    }
+    public void setTransaktionsstatus(String transaktionsstatus) {
+        this.transaktionsstatus = transaktionsstatus;
+    }
 }
