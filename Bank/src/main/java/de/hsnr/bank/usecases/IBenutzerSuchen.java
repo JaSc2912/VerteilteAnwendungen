e9326c9d7cfa180510acdf6fa8176bc81e1cf746
Jan Niklas Schreder <janniklas.schreder@gmx.de>
2025-06-13 11:10:20 +0200

    Added Usecases

---
+++ Bank/src/main/java/de/hsnr/bank/usecases/IBenutzerSuchen.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package de.hsnr.bank.usecases;
+
+import java.util.List;
+
+/**
+ *
+ * @author jannn
+ */
+public interface IBenutzerSuchen {
+    public List<Benutzer> searchBenutzer();
+}
---
+++ Bank/src/main/java/de/hsnr/bank/usecases/TransaktionDurchführen.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package de.hsnr.bank.usecases;
+
+import de.hsnr.bank.usecases.ITransaktionDurchführen;
+
+/**
+ *
+ * @author jannn
+ */
+public class TransaktionDurchführen implements ITransaktionDurchführen {
+    
+}
