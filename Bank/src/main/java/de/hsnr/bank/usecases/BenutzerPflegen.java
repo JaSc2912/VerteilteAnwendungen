ac3b88792390dda01031feebc9b3e330e48b8f51
Jan Niklas Schreder <janniklas.schreder@gmx.de>
2025-06-13 09:57:04 +0200

    BenutzerPflegen and Rolle T

---
+++ Bank/src/main/java/de/hsnr/bank/usecases/BenutzerPflegen.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package de.hsnr.bank.usecases;
+
+/**
+ *
+ * @author jannn
+ */
+public class BenutzerPflegen implements IBenutzerPflegen {
+
+    @Override
+    public boolean addBenutzer(String benutzername, String name, String passwort, String telefonnummer, RolleT rolle) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean deleteBenutzer(String benutzername) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean editBenutzer(String benutzername) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ Bank/src/main/java/de/hsnr/bank/usecases/IBenutzerPflegen.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package de.hsnr.bank.usecases;
+
+/**
+ *
+ * @author jannn
+ */
+public interface IBenutzerPflegen {
+    boolean addBenutzer(String benutzername, String name, String passwort, String telefonnummer, RolleT rolle);
+    boolean deleteBenutzer(String benutzername);
+    boolean editBenutzer(String benutzername);
+}
