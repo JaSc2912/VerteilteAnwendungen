1d9d9a7c19f7359d1a3d5702a93376f47d510209
Jan Niklas Schreder <janniklas.schreder@gmx.de>
2025-06-13 09:20:43 +0200

    AddedEntities

---
+++ Bank/src/main/java/de/hsnr/bank/BenutzerEntity.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package de.hsnr.bank;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import java.io.Serializable;
+
+/**
+ *
+ * @author jannn
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class BenutzerEntity implements Serializable {
+
+   
+}
