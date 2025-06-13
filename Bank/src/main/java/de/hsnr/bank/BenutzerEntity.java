840ced0593594ab3aac06b5fa0f05ef5209221c4
Nemrac724 <tobias.buchner@stud.hn.de>
2025-06-13 10:00:11 +0200

    Revert "Merge branch 'main' of https://github.com/JaSc2912/VerteilteAnwendungen"
    This reverts commit a292b3db05dd44fba15abe31405b50c25f9ea5d5, reversing
    changes made to 50bb0804df1bfac063bf3577cad11b9f4324cee0.

--- Bank/src/main/java/de/hsnr/bank/BenutzerEntity.java
+++ Bank/src/main/java/de/hsnr/bank/BenutzerEntity.java
@@ -4,9 +4,7 @@
  */
 package de.hsnr.bank;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import java.io.Serializable;
@@ -19,17 +17,6 @@ import java.io.Serializable;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class BenutzerEntity implements Serializable {
 
-
-    @Column(name="BENUTZERKENNUNG")
-    @Id
-    private String benutzername;
-    @Column(name="PASSWORT")
-    private String passwort;
-   @Column (name="NAME")
-   String name;
-   @Column (name="TELEFONNUMMER")
-   private String telefonnummer;
-   @Column (name="ROLLE")
-   String rolle;
-
+   private String Benutzername;
+   private String  Passwort;
 }
